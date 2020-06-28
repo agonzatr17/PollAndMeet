175d4ef5dc16b47627a029a87e025dfb2d900a06
agonzatr17 <alfredgt94@hotmail.com>
2020-06-28 22:56:40 +0200

    [7] Send tts response back to phone, add try-catch and logs

--- api/VonageController.js
+++ api/VonageController.js
@@ -1,48 +1,59 @@
-const { getPhonesList, displayInboundSms, isOriginalPhoneNumber, displayInboundDtmf } = require("./phone-utils");
+const { getPhonesList, displayInboundSms, isOriginalPhoneNumber, displayInboundDtmf, getDtmfResponse } = require("./phone-utils");
 const { sendSms, sendResponseSms } = require("./send-sms");
 const { sendTts } = require("./send-tts");
 const { WEBHOOKS_DTMF } = require("../config/urls");
 
 module.exports = {
     handleInboundSms: function(request, response) {
-        var params = Object.assign(request.query, request.body);
-        displayInboundSms(params);
-        response.status(204).send();
+        try {
+            var params = Object.assign(request.query, request.body);
+            displayInboundSms(params);
+            response.status(204).send();
 
-        var sender = params.msisdn;
-        var message = params.text;
+            var sender = params.msisdn;
+            var message = params.text;
 
-        if (isOriginalPhoneNumber(sender)) {
-            var phones = getPhonesList();
-            phones.forEach( phone => {
-                sendSms(phone, message);
-                sendTts(phone, message, [`${request.protocol}://${request.get('host')}${WEBHOOKS_DTMF}`]);
-            });
-        } else {
-            sendResponseSms(sender, message);
+            if (isOriginalPhoneNumber(sender)) {
+                var phones = getPhonesList();
+                phones.forEach( phone => {
+                    sendSms(phone, message);
+                    sendTts(phone, message, [`${request.protocol}://${request.get('host')}${WEBHOOKS_DTMF}`]);
+                });
+            } else {
+                sendResponseSms(sender, message);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send();
         }
     },
     onInputResponse: function(request, response) {
-        var params = Object.assign(request.query, request.body);
-        displayInboundDtmf(params);
-        response.status(204).send();
+        try {
+            var params = Object.assign(request.query, request.body);
+            displayInboundDtmf(params);
+            var sender;
+            var dtmfResponse;
 
-        /* TODO: send response back to original phone, how to get sender¿? using uuid from tts? 
-            -> the inboundDtmf has a from and to parameters (use the to param to know the other phone)
-        var sender = ??;
-        var dtmfResponse = params.dtmf.digits;
+            try {
+                sender = params.to;
+                dtmfResponse = getDtmfResponse(params.dtmf.digits);
+            } catch (error) {
+                console.log("400 Bad Request: response from user not found");
+                console.error(error);
+                response.status(400).send();
+                return;
+            }
 
-        sendResponseSms(sender, dtmfResponse);*/
+            if (sender && dtmfResponse) {
+                response.status(204).send();
+                sendResponseSms(sender, dtmfResponse);
+            } else {
+                console.log("400 Bad Request: response from user not found");
+                response.status(400).send();
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send();
+        }
     }
 }
-
-/*Inbound DTMF:
-{
-    speech: { error: 'Speech was not enabled' },
-    dtmf: { digits: '1', timed_out: false },
-    from: 'NEXMO phone',
-    to: 'the phone number that received the tts message',
-    uuid: '---',
-    conversation_uuid: '---',
-    timestamp: '2020-06-28T18:01:06.265Z'
-}*/
--- config/routes.js
+++ config/routes.js
@@ -2,23 +2,8 @@ const { handleInboundSms, onInputResponse } = require("../api/VonageController")
 const { WEBHOOKS_INBOUND_SMS, WEBHOOKS_DTMF } = require("./urls");
 
 module.exports = function(app){
-    app.get(WEBHOOKS_INBOUND_SMS, function(req, res) {
-        try {
-            handleInboundSms(req, res);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send();
-        }
-    });
-    /*app.post('/webhooks/inbound-sms', function(req, res){
-        handleInboundSms(req, res);
-    });*/
-    app.post(WEBHOOKS_DTMF, function(req, res) {
-        try {
-            onInputResponse(req, res);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send();
-        }
-    });
+    app
+        .get(WEBHOOKS_INBOUND_SMS, handleInboundSms)
+        .post(WEBHOOKS_INBOUND_SMS, handleInboundSms)
+        .post(WEBHOOKS_DTMF, onInputResponse);
 }
